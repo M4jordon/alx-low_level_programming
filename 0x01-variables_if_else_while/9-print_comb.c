39652ca8ac339a22e4fcfdca2bb8997db8a52819
Okoronkwo James <okoronkwo.james@yahoo.com>
2023-02-19 13:21:14 -0800

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0_to_9 seperated by commas, using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
