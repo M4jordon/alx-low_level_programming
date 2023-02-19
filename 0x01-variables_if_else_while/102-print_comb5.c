acf903d10c8ba0702b6cc6b39d327e20713626a5
Okoronkwo James <okoronkwo.james@yahoo.com>
2023-02-19 14:59:46 -0800

    double combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - print double num pair '00 01' - '99 98' witout duplicates
+ * Return: 0
+ */
+
+int main(void)
+{
+	int tens;
+	int ones;
+	int n;
+	int i;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			for (n = tens; t <= '9'; n++)
+			{
+				for (i = ones; i <= '9'; i++)
+				{
+					putchar(tens);
+					putchar(ones);
+					putchar(n);
+					putchar(i);
+
+					if (!((tens == '9' && ones == '9') && (n == '9' && i == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
