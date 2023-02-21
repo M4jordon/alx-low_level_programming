2d1cbce193d44a6e005d2720734779d688b3b5d5
Okoronkwo James <okoronkwo.james@yahoo.com>
2023-02-20 20:19:22 -0800

    software working

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,27 @@
 #include <stdio.h>
-
 /**
- * main - print double num pair '00 01' - '99 98' witout duplicates
- * Return: 0
- */
-
+ * main - prints all possible combinations of two two-digits numbes,
+ * Ranging from 0-99, separated by a comma followed by a space. 
+ *
+ * Return: Always 0.
+**/
 int main(void)
 {
-	int tens;
-	int ones;
-	int n;
-	int i;
+	int num1, num2;
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (num1 = 0; num1 <=98; num1++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (num2 = num1 + 1; num2 <=99; num2++)
 		{
-			for (n = tens; n <= '9'; n++)
-			{
-				for (i = ones + 1; i <= '9'; i++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(n);
-					putchar(i);
-
-					if (!((tens == '9' && ones == '9') && (n == '9' && i == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
