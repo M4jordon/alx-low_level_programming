3c722f9b97172f29a5d60824c165efc8bed20bc7
Okoronkwo James <okoronkwo.james@yahoo.com>
2023-02-22 21:31:28 -0800

    library

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,88 @@
+#define _MAIN_H_
+#ifndef _MAIN_H_
+
+/**
+ * _putchar - writes the character d to stdout
+ * @d: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and the errno is set appropriately.
+ */
+
+int _putchar(char d);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
+void print_alphabet(void);
+/**
+ * print_alphabet_x10 - print lowercase alphabet
+ */
+
+void print_alphabet_x10(void);
+
+/**
+ * _islower - checks for lowercase
+ * @d: the chracter to check
+ * Return: int
+ */
+
+int _islower(int d);
+
+/**
+ * _isalpha - checks for alphabet character
+ * @d: character to check
+ * Return: 1 if lower or uppercase, 0 if not alphabet character
+ */
+
+int _isalpha(int d);
+
+/**print_sign - print sign of number
+ * @n: number to check
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+ */
+
+int print_sign(int n);
+/**
+ * _abs - computer absolute value of integer
+ * @int: integer
+ * Return: absolute value
+ */
+int_abs(int);
+
+/**
+ * print_last_digit - print last digit of integer
+ * @int: integer
+ * Return: last digit of number
+ */
+int print_last_digit(int);
+
+/**]
+ * jack_bauer - print every minute of a day
+ * Description: print minutes 00:00 to 23:59
+ */
+
+void jack_bauer(void);
+
+/**
+ * times_table - print multiplication table
+ */
+
+void times_table(void);
+
+/**
+ * print_to_98 - print numbers n to 98
+ * @n: integer argument
+ */
+
+void print_to_98(int n);
+
+/**
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
+ */
+
+void print_time_table(int n);
+
+#endif
