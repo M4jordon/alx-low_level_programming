b396bec190b1250ee5b6e31d03c7d232ed8ff492
Okoronkwo James <okoronkwo.james@yahoo.com>
2023-02-24 20:11:16 -0800

    leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,19 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 
 void positive_or_negative(int i);
-
-
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
