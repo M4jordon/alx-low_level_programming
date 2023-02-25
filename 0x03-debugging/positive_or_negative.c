c50fd50c24a2a76de84a4de877f03de20c393c87
Okoronkwo James <okoronkwo.james@yahoo.com>
2023-02-24 16:07:40 -0800

    positive

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,5 +7,5 @@ void positive_or_negative(int i)
         if (i < 0)
                 printf("%d is negative number\n", i);
         else
-                printf("%d is postivie number\n", i);
+                printf("%d is positive number\n", i);
 }
